752a01722dd025ac4925dc0fa7a44e4555b825a4
Ahmad Muhammad Salisu <ahmadsalisu.ams@gmail.com>
2025-07-23 16:58:54 +0100

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -6,7 +6,7 @@ const { sequelize } = require('./models');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(helmet());
